c1038a2b4579a6cad8389a659b163e5dff700bed
JQM96 <jquintanamartin.games@gmail.com>
2025-11-25 10:47:05 +0100

    Simple jump

--- SideviewCharacterController/Assets/Movement.cs
+++ SideviewCharacterController/Assets/Movement.cs
@@ -3,14 +3,27 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] float speed = 5;
+
+    [Header("Jump")]
+    [SerializeField] float jumpForce = 5;
+    [SerializeField] LayerMask whatIsGround;
+    [SerializeField] Transform groundCheckOrigin;
 
     Rigidbody2D rb;
     Vector2 inputVector;
 
+    bool canJump;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        canJump = Physics2D.Raycast(groundCheckOrigin.position, Vector2.down, 0.1f, whatIsGround);
     }
 
     private void FixedUpdate()
@@ -21,5 +34,14 @@ public class Movement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         inputVector = context.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (canJump == false)
+            return;
+
+        if (context.phase == InputActionPhase.Performed)
+            rb.AddForce(new Vector2(rb.linearVelocityX, jumpForce), ForceMode2D.Impulse);
     }
 }
